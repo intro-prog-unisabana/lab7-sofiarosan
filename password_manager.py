5130f9b7646770dd3c400866e3d6a0afea34a37b
sofiarosan <anarodsan@unisabana.edu.co>
2026-04-13 18:46:54 -0500

    password manager 3

--- password_manager.py
+++ password_manager.py
@@ -29,10 +29,33 @@ def encrypt_passwords_in_file(filename: str) -> None:
       writer.writerows(rows)
 
 def change_password(filename: str, website: str, password: str) -> bool:
-    """TODO: Parte 3."""
-    pass
+    
+    rows=[]
+
+    with open(filename, 'r' ) as f:
+        reader=csv.reader(f)
+
+        for row in reader:
+            if row:
+                rows.append(row)
+    
+    found=False
+
+    for i in range(1, len(rows)):
+        if rows[i][0] == website:
+            rows[i][2]== caesar_encrypt(password)
+            found== True
+            break
+    if not found:
+        return False 
+    
+    with open(filename, 'w', newline ='') as f:
+        writer = csv.writer(f)
+        writer.writerows(rows)
+    return True
 
 
 def add_login(filename: str, website_name: str, username: str, password: str) -> None:
-    """TODO: Parte 4."""
-    pass
+    with open (filename, 'a',newline='') as f:
+        writer= csv.writer(f)
+        writer.writerow([website_name, username, caesar_encrypt(password)])
