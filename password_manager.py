50f2f17b4d4d2ab4d60300ddd47518fbb99e7742
sofiarosan <anarodsan@unisabana.edu.co>
2026-04-13 18:28:20 -0500

    password_manager parte 2

--- password_manager.py
+++ password_manager.py
@@ -4,22 +4,29 @@ from caesar import caesar_encrypt
 
 
 def encrypt_single_pass(filename: str) -> None:
-    with open (filename.txt, 'r') as file:
+    with open (filename.txt, 'r') as f:
 
-        password= file.readline().strip
+        password= f.readline().strip
 
     encrypted=caesar_encrypt(password)
 
-    with open (filename, "w") as file:
-        file.write(encrypted)
+    with open (filename, "w") as f:
+        f.write(encrypted)
 
 
 def encrypt_passwords_in_file(filename: str) -> None:
-    with open(filename, 'r') as file:
-        reader= csv.reader(file )
-        for row in reader:
-            print (row)
+    with open(filename, 'r') as f:
+        reader= csv.reader(f)
+        rows=[]
 
+        for row in reader:
+            rows.append(row)
+    for index, row in enumerate(rows):
+                if index !=0:
+                   row[2] = caesar_encrypt(row[2])
+    with open(filename, 'w') as f: 
+      writer= csv.writer(f)
+      writer.writerows(rows)
 
 def change_password(filename: str, website: str, password: str) -> bool:
     """TODO: Parte 3."""
